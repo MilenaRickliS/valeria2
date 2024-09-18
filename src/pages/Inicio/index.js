60af48321522716dcd15524cd687da2aa7cf7eac
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-09-18 14:24:09 -0300

    cc

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,31 @@
+
+import "./style.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+
+function Inicio() {
+
+
+
+  return (
+    
+      <div>
+        <div><Header/></div>
+        <br/>
+        <br/>
+        <br/>
+        
+       <div>
+
+       </div>
+      
+     
+
+      <Footer/>
+      </div>
+
+  );
+}
+
+export default Inicio;
