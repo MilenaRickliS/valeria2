4a0b49fe1fc3bfde9c3ff770faca6dc0d6590486
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-09-18 23:28:32 -0300

    cc

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -26,11 +26,23 @@ function Inicio() {
         </div>
         
           <div className="flex">
-            <div className="produto">
-
-            </div>
-            <div className="produto">
+            <div className="produto prod1">
               
+              <div className="sobre-produto">
+                <p className="t1">Como Eliminar o Efeito Sanfona no Emagrecimento</p>
+                <p className="t2">Entenda que é possível não comer suas emoções. Essa culpa não é sua!</p>
+                <p className="t2">R$ 27,90</p>
+                <a className="link" href="#">Saiba mais! <i class="bi bi-arrow-right-short"></i></a>
+              </div>
+            </div>
+            <div className="produto prod2">
+            
+              <div className="sobre-produto">
+                <p className="t1">Como Sair do Efeito Sanfona e Nunca Mais Voltar</p>
+                <p className="t2">Entenda que é possível não comer suas emoções. Essa culpa não é sua!</p>
+                <p className="t2">R$ 27,90</p>
+                <a className="link" href="#">Saiba mais! <i class="bi bi-arrow-right-short"></i></a>
+              </div>
             </div>
           </div>
           
