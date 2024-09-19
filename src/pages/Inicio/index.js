3179d14e84bad928668a0595663cf29f24be1e38
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-09-19 15:09:56 -0300

    cc

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -32,8 +32,6 @@ function Inicio() {
                 <p className="t2">Entenda que é possível não comer suas emoções. Essa culpa não é sua!</p>
               </div>
               <div className="sobre-produto">
-                <p className="t1">Como Eliminar o Efeito Sanfona no Emagrecimento</p>
-                <p className="t2">Entenda que é possível não comer suas emoções. Essa culpa não é sua!</p>
                 <p className="t2">R$ 27,90</p>
                 <a className="link" href="#">Saiba mais! <i class="bi bi-arrow-right-short"></i></a>
               </div>
@@ -44,8 +42,6 @@ function Inicio() {
                 <p className="t2">Entenda que é possível não comer suas emoções. Essa culpa não é sua!</p>
               </div>
               <div className="sobre-produto">
-                <p className="t1">Como Sair do Efeito Sanfona e Nunca Mais Voltar</p>
-                <p className="t2">Entenda que é possível não comer suas emoções. Essa culpa não é sua!</p>
                 <p className="t2">R$ 27,90</p>
                 <a className="link" href="#">Saiba mais! <i class="bi bi-arrow-right-short"></i></a>
               </div>
