a614b596d9aab2e0af58a7e08893221246b44f11
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-09-21 23:17:44 -0300

    cc

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -22,23 +22,32 @@ function Sobre() {
         
        <div>
           <img className="img-eu" src={eu}/>
-          <p className="sobre-mim">Valéria Spegel Rickli é uma empreendedora, palestrante e instrutora de academia. Possui mais de 10 anos de experiência na área. 
+          <div className="div-sobre">
+            <p className="sobre-mim">Valéria Spegel Rickli é uma empreendedora, palestrante e instrutora de academia. Possui mais de 10 anos de experiência na área. 
             Publicou o Livro "Como Eliminar o efeito Sanfona no Emagrecimento", <a href="https://valeriarickli.orbitpages.online/como-eliminar-o-efeito-sanfona-no-emagrecimento-2?fbclid=PAZXh0bgNhZW0CMTEAAabIILzpnh65rm8pz0efQxbpy9HQih9oFD1EzANLsNh8LacnweqqTnVkw6g_aem_mGJTFQNp0nzcLzObeGIAaw">confira aqui!</a> Descobriu o porque de muitas pessoas 
             entrarem em um looping, de emagrecer e engordar, e nunca chegar ao que espera! Por isso, decidiu compartilhar sua experiência e conhecimento, a guiar mais pessoas a uma vida mais saudável (mentoria e saúde).</p>
   
+          </div>
+          
+          <div className="contato">
+            <a href=""><i class="bi bi-whatsapp"></i></a>
+            <a href=""><i class="bi bi-instagram"></i></a>
+            <a href=""><i class="bi bi-envelope-at-fill"></i></a>
+            <a href=""><i class="bi bi-youtube"></i></a>
+          </div>
+          
        </div>
        <div className="footer">
        <div className="footer-nome">
           
           <p>Valéria Spegel Rickli</p>
-          <p>CNPJ 23.756.789/0001-56</p><br></br>
-          <a href="/">Voltar a Loja</a>
+          <p><i class="bi bi-c-circle"></i>Todos os Direitos Reservados</p><br></br>
+          
         </div>
         <div className="footer-contato">
           <p>Dúvidas ou quer falar comigo?</p>
-          <p>Aqui está meu Email e WhatsApp, entre em contato! </p><br></br>
-          <a href="">@email</a>
-          <a href=""><i class="bi bi-whatsapp"></i></a>
+          <p>Acima está meu Email e WhatsApp, entre em contato! </p><br></br>
+          
         </div> 
 
        </div>   
