a78a406498e648cc407dc308beea330ce5419d6d
MilenaRickliS <milena.silverio2506@gmail.com>
2025-02-14 20:44:26 -0300

    cc

---
+++ src/pages/Pagamento/index.js
@@ -0,0 +1,22 @@
+
+import "./style.css";
+import logo from '../../assets/68L_Valéria Spegel Rickli_Prancheta 1 cópia.png';
+
+
+function Pagamento() {
+
+  return (
+    
+      <div>
+        <img className="logo" src={logo}/>   
+        
+       
+     
+
+      
+      </div>
+
+  );
+}
+
+export default Pagamento;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 
 import Inicio from '../pages/Inicio';
 import Sobre from '../pages/Sobre';
+import Loja from '../pages/Loja';
+import Pagamento from '../pages/Pagamento';
 
 
 
@@ -11,6 +13,10 @@ function RoutesApp(){
       <Route path="/" element={<Inicio/>}/>
 
       <Route path="/sobre" element={ <Sobre/> } />
+
+      <Route path="/Loja" element={<Loja/>}/>
+
+      <Route path="/Pagamento" element={<Pagamento/>}/>
 
     
     </Routes>
