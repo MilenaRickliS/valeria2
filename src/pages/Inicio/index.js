5bfcb3ad85238ef3d28ba3a1b0fa74e6ab959250
MilenaRickliS <milena.silverio2506@gmail.com>
2025-02-21 17:04:15 -0300

    cc

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -4,55 +4,10 @@ import logo from '../../assets/68L_Valéria Spegel Rickli_Prancheta 1 cópia.p
 import frase from '../../assets/efeito-sanfona.png';
 
 function Inicio() {
-
-
-
-  return (
-    
+  return (    
       <div>
         
-       <nav className="menu">
-          <ul>
-            <li><a href="#">Loja</a></li>
-            <li><a href="/sobre">Sobre mim</a></li>
-          </ul>
-          <img className="logo" src={logo}/>
-       </nav>
-        
-       <div>
-        <div className="titulo">
-          <h1>Bem-vindo(a)!</h1>
-          <h2>Você está um passo da sua compra!</h2>
-        </div>
-        
-          <div className="flex container">
-            <div className="produto prod1">
-              <div className="sobre-produto1">
-                <p className="t1">Como Eliminar o Efeito Sanfona no Emagrecimento</p>
-                <p className="t2">Entenda que é possível não comer suas emoções. Essa culpa não é sua!</p>
-              </div>
-              <div className="sobre-produto">
-                <p className="t2">R$ 27,90</p>
-                <a className="link" href="https://valeriarickli.orbitpages.online/como-eliminar-o-efeito-sanfona-no-emagrecimento-2?fbclid=PAZXh0bgNhZW0CMTEAAabIILzpnh65rm8pz0efQxbpy9HQih9oFD1EzANLsNh8LacnweqqTnVkw6g_aem_mGJTFQNp0nzcLzObeGIAaw">Saiba mais! <i class="bi bi-arrow-right-short"></i></a>
-              </div>
-            </div>
-            <div className="produto prod2">
-            <div className="sobre-produto1">
-                <p className="t1">Como Sair do Efeito Sanfona e Nunca Mais Voltar</p>
-                <p className="t2">Venha conhecer o que mudou na minha vida e mude a sua também! (Mentoria de 6 meses)</p>
-              </div>
-              <div className="sobre-produto">
-                <p className="t2">R$ 4997,00</p>
-                <a className="link" href="#">Saiba mais! <i class="bi bi-arrow-right-short"></i></a>
-              </div>
-            </div>
-          </div>
-          
-          
-       </div>
-       <div className="flex">
-        <img  className="frase" src={frase}/>
-       </div>
+
        
      
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Inicio from '../pages/Inicio';
-import Sobre from '../pages/Sobre';
-import Loja from '../pages/Loja';
 import Pagamento from '../pages/Pagamento';
-
 
 
 function RoutesApp(){
   return(
     <Routes>
       <Route path="/" element={<Inicio/>}/>
-
-      <Route path="/sobre" element={ <Sobre/> } />
-
-      <Route path="/Loja" element={<Loja/>}/>
 
       <Route path="/Pagamento" element={<Pagamento/>}/>
 
