93a5a059a94ca215b55436fd0a44893663bbbff6
MilenaRickliS <milena.silverio2506@gmail.com>
2025-02-21 18:00:33 -0300

    cc

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -6,11 +6,74 @@ import frase from '../../assets/efeito-sanfona.png';
 function Inicio() {
   return (    
       <div>
-        
+        <img className="logo" src={logo}/>   
+        <h1>Mentoria Construindo sua Saúde!</h1>
+        <h2>Com está mentoria você vai poder:</h2>
+        <ul>
+          <li></li> 
+          <li></li>
+          <li></li> 
+          <li></li>
+        </ul> 
 
-       
-     
+        <button>QUERO A MENTORIA</button>
+        <button></button>
+        <button></button>
 
+        <div>
+          <div>
+            <p>Antes da Mentoria</p>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div>
+            <p>Depois da Mentoria</p>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+
+        <button>QUERO A MENTORIA</button>
+        <button></button>
+        <button></button>
+
+        <img></img>
+
+        <h2>O que você recebe:</h2>
+        <ul>
+          <li><p></p></li> 
+          <li><p></p></li> 
+          <li><p></p></li>  
+          <li><p></p></li> 
+        </ul>
+
+        <img></img>
+
+        <h2>Experimente primeiro e decida depois</h2>
+        <p></p>
+
+        <button>QUERO A MENTORIA</button>
+        <button></button>
+        <button></button>
+
+        <h2>Pessoas que embarcaram comigo nesta mentoria</h2>
+        <img></img>
+        <button>QUERO A MENTORIA</button>
+        <button></button>
+        <button></button>
+
+        <h2>Quem será sua professora?</h2>
+        <img></img>
+        <p></p>
+        <button>QUERO A MENTORIA</button>
+        <button></button>
+        <button></button>
       
       </div>
 
--- src/pages/Pagamento/index.js
+++ src/pages/Pagamento/index.js
@@ -8,12 +8,92 @@ function Pagamento() {
   return (
     
       <div>
-        <img className="logo" src={logo}/>   
-        
-       
-     
+        <img className="logo" src={logo}/>
 
-      
+        <div>
+          <div>
+            <img></img>
+            <div>
+              <h3>Titulo</h3>
+              <p>Autor</p>
+              <p>preço</p>
+              <p>parcelamento</p>
+            </div>
+          </div>
+
+          <form>
+
+          </form>
+
+          <div>
+            <div>
+              <h3></h3>
+              <p></p>
+            </div>
+            <div>
+              <h3></h3>
+              <p></p>
+            </div> 
+            <div>
+              <h3></h3>
+              <p></p>
+            </div>   
+          </div> 
+
+          <h3>Aproveite e compre junto:</h3>
+
+          <div>
+            <img></img>
+            <div>
+              <h4></h4>
+              <p></p>
+              <p></p>  
+            </div>  
+          </div> 
+
+          <h3>Detalhes da compra</h3>
+          <hr></hr>
+          <div>
+            <p></p>
+            <p></p>
+          </div> 
+
+          <button></button>
+
+          <p></p>     
+        
+        </div>   
+
+        <div>
+          <div>
+            <img></img>
+            <div>
+              <p>Privacidade</p>
+              <p>Sua informação 100% segura</p>
+            </div>
+          </div> 
+          <div>
+            <img></img>
+            <div>
+              <p>Compra Segura</p>
+              <p>Ambiente seguro </p>
+            </div>
+          </div> 
+          <div>
+            <img></img>
+            <div>
+              <p></p>
+              <p></p>
+            </div>
+          </div> 
+          <div>
+            <img></img>
+            <div>
+              <p></p>
+              <p></p>
+            </div>
+          </div>           
+        </div>   
       </div>
 
   );
