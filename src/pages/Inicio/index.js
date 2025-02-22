63c13fdb78561384f3b252cbd87acdfb0633ea9e
Milena Rickli Silvério <141282223+MilenaRickliS@users.noreply.github.com>
2025-02-21 21:17:22 -0300

    Update index.js

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -10,10 +10,10 @@ function Inicio() {
         <h1>Mentoria Construindo sua Saúde!</h1>
         <h2>Com está mentoria você vai poder:</h2>
         <ul>
-          <li></li> 
-          <li></li>
-          <li></li> 
-          <li></li>
+          <li>Compreender os gatilhos que levam à procrastinação e alimentação inadequada</li> 
+          <li>Melhor qualidade do sono</li>
+          <li>Desenvolver hábitos que pormovem o bem-estar e saúde</li> 
+          <li>Identificar e superar crenças que impedem o seu progresso</li>
         </ul> 
 
         <button>QUERO A MENTORIA</button>
@@ -24,17 +24,17 @@ function Inicio() {
           <div>
             <p>Antes da Mentoria</p>
             <ul>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>Procrastinação frequente e falata de motivação</li>
+              <li>Alimentação desregulada e emocional</li>
+              <li>Baixa qualidadde de sono e cansaço constante</li>
             </ul>
           </div>
           <div>
             <p>Depois da Mentoria</p>
             <ul>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>Aumento da motivação e foco em objetivos de saúde</li>
+              <li>Superação de crenças limitantes e desenvolvimento pessoal</li>
+              <li>Melhora no sono e na alimentação</li>
             </ul>
           </div>
         </div>
@@ -47,10 +47,12 @@ function Inicio() {
 
         <h2>O que você recebe:</h2>
         <ul>
-          <li><p></p></li> 
-          <li><p></p></li> 
-          <li><p></p></li>  
-          <li><p></p></li> 
+          <li>Ferramentas práticas para reprogramação mental<p></p></li> 
+          <li>Estratégias para melhorar a qualidade do sono<p></p></li> 
+          <li>Mentoria detalhada e explicativa<p></p></li>
+          <li>Suporte emocional e motivacional<p></p></li> 
+          <li>Desenvolvido por uma especialista<p></p></li> 
+          <li>Tempo de acesso de 12 meses<p></p></li> 
         </ul>
 
         <img></img>
